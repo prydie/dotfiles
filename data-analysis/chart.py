2c146401eeb82961829dfdf391a26b72e49a3eee
Andrew Pryde <andrew.pryde@oracle.com>
2023-04-19 01:55:29 +0100

    tools

---
+++ data-analysis/chart.py
@@ -0,0 +1,27 @@
+import csv
+import os
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+try:
+    from StringIO import StringIO  # # for Python 2
+except ImportError:
+    from io import StringIO  # # for Python 3
+
+
+def to_df(xlabel, ylabel):
+    return pd.read_csv(sys.stdin, names=[xlabel, ylabel])
+
+def display(title, xlabel, ylabel):
+    df = to_df(xlabel, ylabel)
+    print(df.to_string())
+    df.plot(x=xlabel, y=ylabel, kind="bar")
+
+
+if __name__ == '__main__':
+    title = os.environ.get('TITLE', 'Untitled')
+    xlabel = os.environ.get('X_LABEL', 'X')
+    ylabel = os.environ.get('Y_LABEL', 'Y')
+    display(title, xlabel, ylabel)
