32b222f7ba735ef189437d788cbb6d1dc4a59ef1
Andrew Pryde <andrew.pryde@oracle.com>
2023-05-12 12:03:49 +0100

    Skip snap packages when snap not present

--- install.sh
+++ install.sh
@@ -5,12 +5,9 @@ set -e -u -o pipefail
 IFS=$'\n\t'
 
 # Bootsrap with RCM
-sudo apt install rcm
+sudo apt install rcm  # TODO(apryde) yum
 
 # Make it our own
-make install -k 2>&1 | tee build.log
+make up -k 2>&1 | tee build.log
 
 git config pull.ff only       # fast-forward only
-
-# Make it our own
-make install -k 2>&1 | tee build.log
